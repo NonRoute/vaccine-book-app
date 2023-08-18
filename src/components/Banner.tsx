bee1c7c323239d4986ad0cc757c278a107357b47
NonRoute <chanon.ratn@gmail.com>
2023-08-18 17:40:47 +0700

    Assignment 3 - Create React Web Application

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -5,10 +5,10 @@ export default function Banner() {
 	return (
 		<div className={styles.banner}>
 			<Image src={'/img/cover.jpg'} alt="cover" fill={true} priority objectFit="cover" />
-            <div className={styles.bannerText}>
-                <h1>Vaccine Service</h1>
-                <h3>Build a shield of protection together</h3>
-            </div>
+			<div className={styles.bannerText}>
+				<h1>Vaccine Service</h1>
+				<h3>Build a shield of protection together</h3>
+			</div>
 		</div>
 	)
 }
